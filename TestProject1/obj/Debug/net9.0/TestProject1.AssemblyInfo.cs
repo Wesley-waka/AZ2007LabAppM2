94201fce764e997d8815d0fe95f4f49404630c66
Wesley Waka <wesleywaka2@gmail.com>
2025-08-23 10:10:37 +0300

    Test for Project lib

--- TestProject1/obj/Debug/net9.0/TestProject1.AssemblyInfo.cs
+++ TestProject1/obj/Debug/net9.0/TestProject1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3867f470241df065e4797f2aa4de5edb936c10de")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
